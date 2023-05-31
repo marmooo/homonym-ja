bd48b3c5bba3a9300f0131913c3994a205582522
marmooo <marmooo@users.noreply.github.com>
2023-05-31 09:52:29 +0900

    improve

--- build.js
+++ build.js
@@ -1,25 +1,10 @@
 import { readLines } from "https://deno.land/std/io/mod.ts";
 
 const outPath = "homonym.csv";
-const dicts = {
-  // JmDict: ['JmdictFurigana.txt'],
-  // IPADic: [  // require iconv
-  //   'ipadic-2.7.0/Adverb.utf8.dic',
-  //   'ipadic-2.7.0/Adnominal.utf8.dic',
-  //   'ipadic-2.7.0/Noun.adjv.utf8.dic',
-  //   'ipadic-2.7.0/Noun.adverbal.utf8.dic',
-  //   'ipadic-2.7.0/Noun.verbal.utf8.dic',
-  //   'ipadic-2.7.0/Verb.utf8.dic',
-  // ],
-  NaistJdic: [ // require iconv
-    "mecab-naist-jdic-0.6.3b-20111013/naist-jdic.utf8.csv",
-  ],
-  // UniDic: ['unidic-csj-3.0.1.1/lex.csv'],
-  SudachiDict: [
-    "SudachiDict/src/main/text/small_lex.csv",
-    "SudachiDict/src/main/text/core_lex.csv",
-  ],
-};
+const dicts = [
+  "SudachiDict/src/main/text/small_lex.csv",
+  "SudachiDict/src/main/text/core_lex.csv",
+];
 
 function kanaToHira(str) {
   return str.replace(/[\u30a1-\u30f6]/g, function (match) {
@@ -28,159 +13,70 @@ function kanaToHira(str) {
   });
 }
 
-function getWordFromJmDict(line) {
-  const [word, yomi] = line.split("|", 2);
-  return [word, yomi];
-}
-
-function getWordFromIPADic(line) {
-  const p11 = line.indexOf("見出し語 (") + 6;
-  const p12 = line.slice(p11).indexOf(")");
-  const [word, _cost] = line.slice(p11, p11 + p12).split(" ");
-  const p21 = line.indexOf("(読み ") + 4;
-  const p22 = line.slice(p21).indexOf(")");
-  const yomi = line.slice(p21, p21 + p22).split(" ")[0];
-  return [word, yomi];
-}
-
-function getWordFromNaistJdic(line) {
-  const arr = line.split(",");
-  const pos1 = arr[4];
-  const pos2 = arr[5];
-  const form2 = arr[9];
-  if (pos2 == "固有名詞") {
-    return false;
-  }
-  if (pos1 != "名詞" && form2 != "基本形") {
-    return false;
-  }
-  const word = arr[0];
-  const yomi = arr[11];
-  const variants = arr[13];
-  if (variants != "") {
-    return false;
-  }
-  return [word, yomi, pos1];
-}
-
-function getWordFromUniDic(line) {
-  line = line.replace(/"[^"]+"/g, "");
-  const arr = line.split(",");
-  const pos1 = arr[4];
-  const pos2 = arr[5];
-  const form2 = arr[9];
-  if (pos2 == "固有名詞") {
-    return false;
-  }
-  if (pos1 != "名詞" && form2 != "終止形-一般") {
-    return false;
-  }
-  let word = arr[0];
-  if (word.includes("（") && word.includes("）")) {
-    const tmp = word.split(/[（）]/);
-    word = tmp.slice(1).join("");
-  }
-  let yomi = arr[24];
-  if (yomi == "*") {
-    yomi = arr[10];
-  }
-  return [word, yomi, pos1];
-}
-
-function getWordFromSudachiDict(line) {
+function getWord(line) {
   const arr = line.split(",");
   const surface = arr[0];
-  const word = arr[12];
-  if (surface != word) {
-    return false;
-  }
+  const leftId = arr[1];
   const pos1 = arr[5];
   const pos2 = arr[6];
   const form2 = arr[10];
-  if (pos2 == "固有名詞") {
-    return false;
-  }
-  if (pos1 != "名詞" && form2 != "終止形-一般") {
-    return false;
-  }
   const yomi = arr[11];
-  return [word, yomi, pos1];
+  const word = arr[12];
+  const abc = arr[14];
+  const kanjiRegexp = /[\u4E00-\u9FFF]/g;
+  if (leftId == "-1") return false;
+  if (!kanjiRegexp.test(surface)) return false;
+  if (pos1 == "名詞") {
+    // "わる乗り, 悪のり, 悪乗り" を同音異義語とするのは無理がある
+    // 漢字だけの熟語にすると "開き口, 秋口" などを見逃す
+    // word で正規化しても "顎髭, 顎ひげ" が残る
+    // A 基準に限定すると必要なパターンがかなり失われる
+    // --> 漢字に限定しつつ surface で登録するのが一番マシ
+    if (!/^[\u4E00-\u9FFF]+$/.test(surface)) return false;
+    if (pos2 == "固有名詞") return false;
+    return [surface, yomi];
+  } else {
+    // "割り切れる, 割りきれる, 割切れる" を同音異義語とするのは無理がある
+    // word で正規化すると "挙げる --> 上げる" に正規化される
+    // A 基準に限定すると surface では不要な語句が多すぎる
+    // --> A 基準に限定して word で正規化するのが一番マシ
+    if (form2 != "終止形-一般") return false;
+    if (abc != "A") return false;
+    if (surface != word) return false;
+    return [word, yomi];
+  }
 }
 
-async function addDictData(dictName, path) {
+async function addData(path) {
   const fileReader = await Deno.open(path);
   for await (const line of readLines(fileReader)) {
     if (!line) continue;
-    let data;
-    switch (dictName) {
-      case "IPADic":
-        data = getWordFromIPADic(line);
-        break;
-      case "JmDict":
-        data = getWordFromJmDict(line);
-        break;
-      case "NaistJdic":
-        data = getWordFromNaistJdic(line);
-        break;
-      case "UniDic":
-        data = getWordFromUniDic(line);
-        break;
-      case "SudachiDict":
-        data = getWordFromSudachiDict(line);
-        break;
-      default:
-        console.log("dictName error.");
-    }
+    const data = getWord(line);
     if (data && data[0] && data[1]) {
-      let [word, yomi, pos1] = data;
+      let [word, yomi] = data;
       yomi = kanaToHira(yomi);
       word = kanaToHira(word);
-      // 名詞は漢字のみの熟語、名詞以外は漢字を含む熟語に限定
-      let kanjiCount = 0;
-      let hiraCount = 0;
-      for (let i = 0; i < word.length; i++) {
-        const chr = word[i];
-        if (/[\u4E00-\u9FFF]/.test(chr)) {
-          kanjiCount += 1;
-        } else if (/[ぁ-ん]/.test(chr)) {
-          hiraCount += 1;
+      const words = d[yomi];
+      if (words) {
+        if (!d[yomi].includes(word)) {
+          d[yomi].push(word);
         }
-      }
-      if (
-        (pos1 == "名詞" && hiraCount == 0 && kanjiCount == word.length) ||
-        (pos1 != "名詞" && hiraCount > 0 && kanjiCount > 0)
-      ) {
-        const words = d[yomi];
-        if (words) {
-          if (!d[yomi].includes(word)) {
-            d[yomi].push(word);
-          }
-        } else {
-          d[yomi] = [word];
-        }
+      } else {
+        d[yomi] = [word];
       }
     }
   }
 }
 
 const d = {};
-for (const [dictName, paths] of Object.entries(dicts)) {
-  for (const path of paths) {
-    await addDictData(dictName, path);
-  }
+for (const dict of dicts) {
+  await addData(dict);
 }
-
 for (const [yomi, words] of Object.entries(d)) {
-  if (words.length < 2) {
-    delete d[yomi];
-  }
+  if (words.length < 2) delete d[yomi];
 }
 const arr = Object.entries(d).map(([k, v]) => [k, v]);
-arr.sort(function (a, b) {
-  if (a < b) return -1;
-  if (a > b) return 1;
-  return 0;
-});
+arr.sort();
 
 let result = "";
 for (const [yomi, words] of arr) {
