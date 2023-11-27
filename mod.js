bfdd6a694f04853167d1f5876deaf6799d227949
marmooo <marmooo@users.noreply.github.com>
2023-11-28 00:11:01 +0900

    improve API

--- mod.js
+++ mod.js
@@ -1,36 +1,31 @@
 import { readLines } from "https://deno.land/std/io/mod.ts";
 
 class HomonymJa {
-  static async fetch(url) {
-    const dict = await fetch(url)
-      .then((response) => response.text())
-      .then((text) => {
-        const d = {};
-        text.trimEnd().split("\n").forEach((line) => {
-          const arr = line.split(",");
-          const word = arr[0];
-          const yomis = arr.slice(1);
-          d[word] = yomis;
-        });
-        return d;
-      }).catch((error) => {
-        console.log(error);
-      });
+  static async fetch(url, options) {
+    const response = await fetch(url, options);
+    const text = response.text();
+    const dict = {};
+    text.trimEnd().split("\n").forEach((line) => {
+      const arr = line.split(",");
+      const word = arr[0];
+      const yomis = arr.slice(1);
+      dict[word] = yomis;
+    });
     const homonymJa = new HomonymJa();
     homonymJa.dict = dict;
     return homonymJa;
   }
 
-  static async load(filepath) {
+  static async load(filepath, options) {
     const dict = {};
-    const fileReader = await Deno.open(filepath);
-    for await (const line of readLines(fileReader)) {
+    const file = await Deno.open(filepath, options);
+    for await (const line of readLines(file)) {
       const arr = line.split(",");
       const word = arr[0];
       const yomis = arr.slice(1);
       dict[word] = yomis;
     }
-    fileReader.close();
+    file.close();
     const homonymJa = new HomonymJa();
     homonymJa.dict = dict;
     return homonymJa;
